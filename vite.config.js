ae12093f63773f111f827f9604679f18f2a51269
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-03-10 22:09:20 +0600

    feat: add path alias => @folders/* inside src

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@pages": "/src/pages",
+      "@components": "/src/components",
+      "@utils": "/src/utils",
+      "@assets": "/src/assets",
+      "@context": "/src/context",
+      "@layouts": "/src/layouts",
+      "@stores": "/src/stores",
+      "@features": "/src/features",
+      "@services": "/src/services",
+      "@lib": "/src/lib",
+    },
+  },
+});
