ed0476627eb1477bf0fd5ed84e9a4ad1f54a4b87
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-03-27 21:50:05 +0600

    initial commit

---
+++ src/assets/assets.js
@@ -0,0 +1,15 @@
+import hero_section_banner1 from "./banner1.jpg";
+import hero_section_banner2 from "./banner2.jpg";
+import makeup_category from "./categories/makeup.jpg";
+import fragrance_category from "./categories/fragrance.jpg";
+import hair_care_category from "./categories/hair-care.jpg";
+import skin_care_category from "./categories/skin-care.jpg";
+
+export const assets = {
+  hero_section_banner1,
+  hero_section_banner2,
+  makeup_category,
+  fragrance_category,
+  hair_care_category,
+  skin_care_category,
+};
