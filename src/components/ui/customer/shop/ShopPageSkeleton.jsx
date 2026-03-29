326bf8626b99c0d1436e17b52ffb61898ef03bfa
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-03-29 17:00:55 +0600

    feat: loading skeleton & spinner

--- src/components/ui/customer/shop/ShopPageSkeleton.jsx
+++ src/components/ui/customer/shop/ShopPageSkeleton.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import ProductsSkeleton from "./product/ProductsSkeleton";
 
 const randomWidth = ["w-35", "w-29", "w-31", "w-25", "w-28"];
 
-const ProductSkeleton = () => {
+const ShopPageSkeleton = () => {
   return (
     <div className="flex gap-4 w-full">
       {/* filter section */}
@@ -53,38 +54,11 @@ const ProductSkeleton = () => {
       </div>
 
       {/* Products section */}
-      <div className="w-full md:w-3/4 grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-6 items-center py-4">
-        {Array.from({ length: 10 }).map((_, idx) => {
-          return (
-            <div
-              key={idx}
-              className="bg-white border rounded overflow-hidden cursor-pointer group transition shadow-sm hover:shadow-lg w-full h-85 flex flex-col justify-between"
-            >
-              {/* IMAGE */}
-              <div className="relative w-full h-50 overflow-hidden ">
-                <div className="w-full h-full object-cover bg-gray-200 animate-pulse"></div>
-
-                {/* SALE BADGE */}
-                <span className="absolute top-3 left-3 bg-gray-300 flex w-15 h-4.5 rounded-full animate-pulse"></span>
-              </div>
-
-              {/* CONTENT */}
-              <div className="p-4 flex flex-col items-center gap-3">
-                {/* product title */}
-                <div className="bg-gray-200 w-full h-5 animate-pulse"></div>
-
-                {/* PRICE */}
-                <div className="bg-gray-200 w-1/3 h-4 animate-pulse"></div>
-
-                {/* BUTTON */}
-                <div className=" mt-3 w-full flex h-10 bg-gray-200 rounded animate-pulse"></div>
-              </div>
-            </div>
-          );
-        })}
+      <div className="w-3/4">
+        <ProductsSkeleton length={10} />
       </div>
     </div>
   );
 };
 
-export default ProductSkeleton;
+export default ShopPageSkeleton;
