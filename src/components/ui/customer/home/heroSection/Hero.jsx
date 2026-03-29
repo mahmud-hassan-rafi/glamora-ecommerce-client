8de238221615b921f731a3821c371372c6ee8f88
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-03-29 17:03:33 +0600

    fix: heigh issue on banner

--- src/components/ui/customer/home/heroSection/Hero.jsx
+++ src/components/ui/customer/home/heroSection/Hero.jsx
@@ -4,7 +4,7 @@ import Categories from "./Categories";
 
 const Hero = () => {
   return (
-    <div className="relative flex gap-4 lg:gap-8 w-full sm:h-85 md:h-[60vh] pt-2.5 overflow-hidden repeat-infinite">
+    <div className="relative flex gap-4 lg:gap-8 w-full sm:h-85 md:h-[60vh] max-h-125 pt-2.5 overflow-hidden repeat-infinite">
       <Categories className={"hidden lg:flex flex-col w-75 bg-white h-max"} />
       <HeroSectionSwiper />
     </div>
