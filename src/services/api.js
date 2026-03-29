68506296ee5e77d2bccb1d896751bac99456e850
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-03-29 17:14:23 +0600

    modify: to fetching public data

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,9 @@ export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.VITE_API_BASE_URL,
-    credentials: "include",
+    headers: { "Content-Type": "application/json" },
+    credentials: "omit", // Changed from "include" to "omit" for public APIs
   }),
   endpoints: () => ({}),
+  keepUnusedDataFor: 0,
 });
