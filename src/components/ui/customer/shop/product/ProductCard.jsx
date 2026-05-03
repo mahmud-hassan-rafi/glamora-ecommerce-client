cd85e98556f93e5ec11d59b2bb4e128557f83960
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-05-03 23:09:41 +0600

    feat: Update ShopPageSkeleton and ProductCard for improved layout and loading state

--- src/components/ui/customer/shop/product/ProductCard.jsx
+++ src/components/ui/customer/shop/product/ProductCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import HoverMenu from "./HoverMenu";
 import { useAppContext } from "@context/AppContext";
 import { useDispatch } from "react-redux";
@@ -8,26 +8,31 @@ import { ADD_TO_CART } from "@features/cart/cartSlice";
 const ProductCard = ({ product }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const location = useLocation();
+  const [loaded, setLoaded] = React.useState(false);
 
   const { discountedPrice } = useAppContext();
 
   return (
     <div
-      onClick={() =>
-        navigate(`/shop/${product.id}`, {
-          state: { from: "/shop" },
-        })
-      }
-      className="relative bg-white border rounded overflow-hidden cursor-pointer group transition shadow-sm hover:shadow-lg w-full"
+      onClick={() => navigate(`/shop/${product.id}`)}
+      className="relative bg-white border rounded overflow-hidden cursor-default group transition shadow-sm hover:shadow-lg w-full"
     >
       {/* IMAGE */}
       <div className="relative w-full h-50 overflow-hidden">
-        <img
-          src={product.thumbnail}
-          alt={product.title}
-          loading="lazy"
-          className="w-full h-full object-cover transition duration-300 group-hover:scale-105"
-        />
+        <div>
+          {!loaded && (
+            <div className="size-full absolute inset-0 rounded bg-gray-200 animate-pulse z-1000" />
+          )}
+
+          <img
+            onLoad={() => setLoaded(true)}
+            src={product?.thumbnail}
+            alt={product?.title}
+            loading="lazy"
+            className="w-full h-full object-cover transition duration-300 group-hover:scale-105"
+          />
+        </div>
 
         {/* SALE BADGE */}
         <span className="absolute top-3 left-3 bg-pink-600 text-white text-xs px-3 py-1 rounded-full">
@@ -38,15 +43,16 @@ const ProductCard = ({ product }) => {
       {/* CONTENT */}
       <div className="p-4 flex flex-col items-center gap-2">
         <h5 className="text-gray-800 font-medium text-sm line-clamp-2">
-          {product.title}
+          {product?.title}
         </h5>
 
         {/* PRICE */}
         <div className="flex items-center gap-2">
           <span className="text-gray-400 line-through text-sm">
-            ${product.price}
+            ${product?.price}
           </span>
-          <span className="text-lg font-semibold text-purple-600">
+          {/* gold color text */}
+          <span className="text-lg font-semibold text-[#9e9026]">
             ${discountedPrice(product)}
           </span>
         </div>
@@ -57,13 +63,13 @@ const ProductCard = ({ product }) => {
             e.stopPropagation();
             dispatch(ADD_TO_CART(product));
           }}
-          className=" mt-3 w-full bg-purple-600 text-white py-2 rounded text-sm font-medium transition hover:bg-purple-700"
+          className=" mt-3 w-full bg-emerald-950/80 text-white py-2 rounded text-sm font-medium transition duration-200 hover:bg-emerald-950/90 cursor-pointer"
         >
           Add to Cart
         </button>
       </div>
 
-      <HoverMenu />
+      <HoverMenu product={product} />
     </div>
   );
 };
